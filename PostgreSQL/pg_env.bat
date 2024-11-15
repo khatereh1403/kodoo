1d8a814424c55fb18a7757fdbd8536ed18dee3a7
khatereh1403 <khatereh.company@gmail.com>
2024-11-15 12:16:15 +0330

    Initial commit

---
+++ PostgreSQL/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\Program Files\odoo\odoo\PostgreSQL\bin";%PATH%
+@SET PGDATA=C:\Program Files\odoo\odoo\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=admin
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\Program Files\odoo\odoo\PostgreSQL\share\locale
+
+                            
